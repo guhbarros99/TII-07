0ebd031c7ee1e61b4275a854da785d50d42275b2
Gustavo <gu01stavo@gmail.com>
2025-05-30 22:18:31 -0300

    aula 09

---
+++ 09/model/Cliente.php
@@ -0,0 +1,44 @@
+<?php
+
+class Cliente 
+{
+    private ?int $id; 
+    private string $nome;
+    private string $cpf;
+    private bool $ativo;
+    private ?string $dataDeNascimento;             
+
+    public function __construct(?int $id, string $nome, string $cpf, bool $ativo, ?string $dataDeNascimento )
+    {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->cpf = $cpf;
+        $this->ativo = $ativo;
+        $this->dataDeNascimento = $dataDeNascimento;
+    }
+    
+    public function getId(): int { return $this->id;}
+    public function getNome(): string { return $this->nome;}
+    public function getCpf(): string { return $this->cpf;}
+    public function getAtivo(): int { return $this->ativo;}
+    public function getdataDeNascimento(): string { return $this->dataDeNascimento;} // pergunta besta, pq a Get tem ser com a letra maiuscula
+
+    public function setNome(string $nome) { $this->nome = $nome;}
+    public function setCpf(string $cpf) { $this->cpf = $cpf;}
+    public function setAtivo(bool $ativo) { $this->ativo = $ativo;}
+    public function setdataDeNascimento(string $nascimento) { $this->nome = $nascimento;}
+
+    public function jsonSerialize(): mixed {
+
+    return[
+        'id' => $this->id,
+        'nome' => $this->id,
+        'cpf' => $this->id,
+        'dataDeNascimento' => $this-> dataDeNascimento,
+        'ativo' => $this->ativo,
+        
+    ];
+    }   
+}
+
+
